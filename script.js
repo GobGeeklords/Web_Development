635b0b0c96541277c217860a1b2852af510c15af
Rick Mukherjee <110241119+GobGeeklords@users.noreply.github.com>
2023-09-26 03:38:33 +0530

    Pig Game project

--- script.js
+++ script.js
@@ -1,33 +1,98 @@
 'use strict';
-const model=document.querySelector('.modal');
-const overlay=document.querySelector('.overlay');
-const btn=document.querySelector('.close-modal');
-const btnsOpenModel=document.querySelectorAll('.show-modal');// querySelector only accepts the first element if you have same classes from different elements whereas querySelectorAll selects all elements
-for (let i=0; i<btnsOpenModel.length; i++)
-    btnsOpenModel[i].addEventListener('click', function() {
-        model.classList.remove('hidden');
-        overlay.classList.remove('hidden');
+
+// Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let scores, currentScore, activePlayer, playing;
+
+// Starting conditions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    // 2. Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    // 3. Check for rolled 1
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice;
+      document.getElementById(
+        `current--${activePlayer}`
+      ).textContent = currentScore;
+    } else {
+      // Switch to next player
+      switchPlayer();
+    }
+  }
 });
-const activity=function() { model.classList.add('hidden');
-overlay.classList.add('hidden')
-}
-btn.addEventListener('click',activity);
-overlay.addEventListener('click', activity);
 
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    // scores[1] = scores[1] + currentScore
 
-//if we want to use the same code in mutiple eventlisteners then store the code in a function and call it as an argument in the event listener
-// classes allow us to aggregate multiple css functions in just one container
-// classes allow us to check if the container is present or not
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
 
- //keyboard events are global events we usually list them on the whole document
+    // 2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 100) {
+      // Finish the game
+      playing = false;
+      diceEl.classList.add('hidden');
 
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // Switch to the next player
+      switchPlayer();
+    }
+  }
+});
 
- document.addEventListener('keydown', function(event) {
-    console.log(event.key);// shows which key was pressed
-    if (event.key==='Escape'&& !model.classList.contains('hidden')) {
-                activity();
-            // checks if the class is present or not
-    }     
- });
-
- // 3 types of events for the keyboard 1) keyup: only happens when we lift our finger of the keyboard 2)keydown: happens when we click a button on keyboard 3) keypress: fired continously when we keep our fingers on certain keys
+btnNew.addEventListener('click', init);
