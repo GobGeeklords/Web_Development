fa5d9a4a44fe12d6ab5261f0688005ded2898dd7
Rick Mukherjee <110241119+GobGeeklords@users.noreply.github.com>
2023-09-26 03:24:24 +0530

    Modal features using Javascript

--- script.js
+++ script.js
@@ -1,58 +1,33 @@
 'use strict';
-/*console.log(document.querySelector('.message').textContent);
- document.querySelector('.message').textContent = '👌 Correct Number!';
-
-document.querySelectpr('.number')=13;
-document.querySelector('.score').textContent=20;
-
-
-console.log(document.querySelector('.guess').value);
-document.querySelector('.guess').value=23;*/
-let num=Math.trunc(Math.random() * 20)+1;//always gives a random number between 0 and 1
-let score=10;
-let highscore=0;
-document.querySelector('.check').addEventListener('click', function(){
-    const guess=Number(document.querySelector('.guess').value);
-
-    //wrong input
-    if (!guess) {
-        document.querySelector('.message').textContent = 'No Number!';
-    }
-    // when player wins
-    
-    else if (guess==num){
-        document.querySelector('.message').textContent = '👌 Correct Number!';
-        document.querySelector('.number').textContent=num;
-        document.querySelector('body').style.backgroundColor='#60b347';
-        document.querySelector('.number').style.width = '30rem';// needs to be inside a string
-        if (score>highscore) {
-            highscore=score;
-            document.querySelector('.highscore').textContent=highscore;
-        }
-    }else if(guess!=num){
-        if (score>1) {
-            document.querySelector('.message').textContent =guess>num ? '😧Too High try Again!'  :' 😢Too low try Again!' ;
-            score--;
-            document.querySelector('.score').textContent=score;
-            }else{
-                document.querySelector('.message').textContent='☠️ Sorry You lost';
-                document.querySelector('.score').textContent=0; 
-            }
-    }
+const model=document.querySelector('.modal');
+const overlay=document.querySelector('.overlay');
+const btn=document.querySelector('.close-modal');
+const btnsOpenModel=document.querySelectorAll('.show-modal');// querySelector only accepts the first element if you have same classes from different elements whereas querySelectorAll selects all elements
+for (let i=0; i<btnsOpenModel.length; i++)
+    btnsOpenModel[i].addEventListener('click', function() {
+        model.classList.remove('hidden');
+        overlay.classList.remove('hidden');
 });
+const activity=function() { model.classList.add('hidden');
+overlay.classList.add('hidden')
+}
+btn.addEventListener('click',activity);
+overlay.addEventListener('click', activity);
 
-//adding functionality for again tab
+
+//if we want to use the same code in mutiple eventlisteners then store the code in a function and call it as an argument in the event listener
+// classes allow us to aggregate multiple css functions in just one container
+// classes allow us to check if the container is present or not
+
+ //keyboard events are global events we usually list them on the whole document
 
 
-document.querySelector('.again').addEventListener('click',function(){
-    score=10;
-     num=Math.trunc(Math.random() * 20)+1;
-    document.querySelector('body').style.backgroundColor = ' #222';
-    document.querySelector('.number').style.width = '15rem';
-    const guess=Number(document.querySelector('.guess').value);
-    document.querySelector('.message').textContent = 'Start guessing...';
-    document.querySelector('.score').textContent=score;
-    document.querySelector('.number').textContent='?';
-    document.querySelector('.guess').value='';
-    document.querySelector('.highscore').textContent=highscore;
-});
+ document.addEventListener('keydown', function(event) {
+    console.log(event.key);// shows which key was pressed
+    if (event.key==='Escape'&& !model.classList.contains('hidden')) {
+                activity();
+            // checks if the class is present or not
+    }     
+ });
+
+ // 3 types of events for the keyboard 1) keyup: only happens when we lift our finger of the keyboard 2)keydown: happens when we click a button on keyboard 3) keypress: fired continously when we keep our fingers on certain keys
